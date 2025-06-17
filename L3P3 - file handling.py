9ec0869457ca9b00f5c72b0cac4e6ea426f64283
EliorRonen <Ronenelior2014@gmail.com>
2025-06-17 18:52:48 +0300

    hello

--- L3P3 - file handling.py
+++ L3P3 - file handling.py
@@ -1,13 +1,21 @@
-file1 = open('Codingal.txt', 'r')
-file2 = open('CodingalUpdated.txt', 'w')
-
-for line in file1.readlines():
-    if not (line.startswith('Coding')):
-
-        print(line)
-
-        file2.write(line)
+new_file = open('NewFile.txt','x')
+new_file.close()
 
 
-file1.close()
-file2.close()
+import os
+print("Checking if my_file exists or not....")
+if os.path.exists("my_file.txt"):
+    os.remove("my_file.txt")
+else:
+    print("The file does not exist")
+
+
+my_file = open("my_file.txt", "w")
+my_file.write("HI! I am penguin and I am 1 yr old")
+my_file.close()
+
+
+os.remove('Codingal.txt')
+
+
+os.rmdir("folderrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr.py")
